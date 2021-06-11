75e1df1c0d39febd4878a53f54e1316f7da15d96
NoteBookMint <hodori991004@gmail.com>
2021-06-12 00:11:02 +0900

    Ready to dev

--- src/com/pro02/reservation/Login.java
+++ src/com/pro02/reservation/Login.java
@@ -3,9 +3,26 @@ package com.pro02.reservation;
 public class Login {
     private String inputID;
     private String inputPW;
+    // Correct Value
+    private String UserID;
+    private String UserPasswd;
 
     public void setValue(String inputID, String inputPW) {
         this.inputID = inputID;
         this.inputPW = inputPW;
+    }
+
+    // Call Account
+    public void CallUser(String inputID) {
+
+    }
+
+    // Check Invaild
+    public boolean isValidAccount() {
+        boolean isVaild = false;
+
+        // TODO: SET LOGIN SYSTEM
+
+        return isVaild;
     }
 }
