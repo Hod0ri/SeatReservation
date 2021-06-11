5ef763dc39cc143faa85d2dbf37ec222afd58f44
NoteBookMint <hodori991004@gmail.com>
2021-06-11 22:15:54 +0900

    Add User Class
    Now We'll try handling database to class

---
+++ src/com/pro02/reservation/User.java
@@ -0,0 +1,10 @@
+package com.pro02.reservation;
+
+public class User {
+
+    // Call to data.db User Table
+    private int userNo;
+    private String userName;
+    private String userID;
+    private String userPasswd;
+}
