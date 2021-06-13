11ee9e90b4c563f4a8ea6234893b44a30f9fb72c
GHK <rjsgh0810@naver.com>
2021-06-13 18:18:33 +0900

    Ready to Dev(Add Register.java)
    Declare field variable

---
+++ src/com/pro02/reservation/Register.java
@@ -0,0 +1,21 @@
+package com.pro02.reservation;
+
+public class Register {
+    private String userName;
+    private String userID;
+    private String userPW;
+
+    private void addUser(String userName, String userID, String userPW){
+
+    }
+
+    public void callSQL(){
+
+    }
+
+    public void setInfo(String userName, String userID, String userPW){
+        this.userName = userName;
+        this.userID = userID;
+        this.userPW = userPW;
+    }
+}
