9a3fb3763199c35ce476c013ed67d25b81657ead
NoteBookMint <hodori991004@gmail.com>
2021-06-14 10:11:56 +0900

    Link Database to Register
    But has Result Issue, We'll resovle it soon

--- src/com/pro02/reservation/Register.java
+++ src/com/pro02/reservation/Register.java
@@ -1,12 +1,17 @@
 package com.pro02.reservation;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Register {
     private String userName;
     private String userID;
     private String userPW;
 
-    private void addUser(String userName, String userID, String userPW){
-
+    private ResultSet addUser(String userName, String userID, String userPW) throws SQLException, ClassNotFoundException {
+        LinkDB database = new LinkDB();
+        ResultSet resultSet = database.RunSQL("INSERT INTO User(userName, userID, userPassWD) VALUES (\"" + userName + "\", \""+ userID +"\", \""+ userPW +"\")");
+        return resultSet;
     }
 
     public void callSQL(){
@@ -18,4 +23,5 @@ public class Register {
         this.userID = userID;
         this.userPW = userPW;
     }
+
 }
